bc9c664b3bab8b83f9b99dcbf379e128ba96a142
Roy-Kathurima <roykimathi922@gmail.com>
2025-11-23 05:33:39 +0300

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-please-change')
-    # If DATABASE_URL provided (Render Postgres), SQLAlchemy will use it. Otherwise local SQLite file.
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "sqlite:///pestwatch.db"
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret123")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
-    MAX_CONTENT_LENGTH = 8 * 1024 * 1024
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'adminpass')
