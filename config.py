81395195dd3e7c746d1a6cbb858c964b6b8901d9
Roy-Kathurima <roykimathi922@gmail.com>
2025-11-23 05:41:22 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret123")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "devkey123")
+
+    # Uploads folder
+    UPLOAD_FOLDER = os.path.join(basedir, "uploads")
+
+    # SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
