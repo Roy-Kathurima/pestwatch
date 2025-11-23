df650c3d8ad87f87072910b6e56133751133eace
Roy-Kathurima <roykimathi922@gmail.com>
2025-11-23 16:00:43 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,19 +1,6 @@
-from app import create_app
+from app import app
 from models import db
-from config import Config
-import os
-
-app = create_app()
 
 with app.app_context():
-    print("Creating database tables (if not exist)...")
     db.create_all()
-    print("Done.")
-    # optional: create a demo admin user (not necessary—admin uses password)
-    # optional: create a demo farmer for testing
-    # from models import User
-    # if not User.query.filter_by(email="demo@example.com").first():
-    #     u = User(fullname="Demo Farmer", email="demo@example.com")
-    #     u.set_password("password123")
-    #     db.session.add(u)
-    #     db.session.commit()
+    print("Database tables created.")
