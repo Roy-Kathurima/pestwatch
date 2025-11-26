0793c702dd16de31c09102ce9d4e866e23d7bf05
Roy-Kathurima <roykimathi922@gmail.com>
2025-11-26 22:49:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ class Report(db.Model):
 # ===========================
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("welcome.html")
 
 
 # ===========================
@@ -185,3 +185,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
