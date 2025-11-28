8fb7b22ff5976da30a426c6958e3f1d02f4303d5
Roy-Kathurima <roykimathi922@gmail.com>
2025-11-28 23:42:47 +0300

    Add files via upload

---
+++ pestwatch/config.py
@@ -0,0 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret")
+    # Use DATABASE_URL env var if set (e.g. postgres), else default to sqlite
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "sqlite:///" + os.path.join(basedir, "pestwatch.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, "static", "uploads")
+    # Alert parameters (can tweak)
+    ALERT_WINDOW_DAYS = int(os.environ.get("ALERT_WINDOW_DAYS", 7))
+    ALERT_RADIUS_KM = float(os.environ.get("ALERT_RADIUS_KM", 5.0))
+    ALERT_THRESHOLD = int(os.environ.get("ALERT_THRESHOLD", 3))  # # reports to trigger
