047cb058b4a6b7e8ef02005a18fef883d3e830f6
Roy-Kathurima <roykimathi922@gmail.com>
2025-12-03 03:29:52 +0300

    Update geolocation.js

--- pestwatch/static/js/geolocation.js
+++ pestwatch/static/js/geolocation.js
@@ -1,6 +1,10 @@
-function getLocation() {
-  navigator.geolocation.getCurrentPosition(pos => {
-    document.getElementById("lat").value = pos.coords.latitude;
-    document.getElementById("lng").value = pos.coords.longitude;
-  });
+function useDeviceLocation() {
+  if (!navigator.geolocation) { alert('Geolocation not supported'); return; }
+  navigator.geolocation.getCurrentPosition(function(pos){
+    document.getElementById('lat').value = pos.coords.latitude;
+    document.getElementById('lng').value = pos.coords.longitude;
+    if (typeof deviceMarker !== 'undefined' && deviceMarker) {
+      deviceMarker.setLatLng([pos.coords.latitude, pos.coords.longitude]);
+    }
+  }, function(err){ alert('Unable to retrieve location: ' + err.message); }, { enableHighAccuracy:true });
 }
