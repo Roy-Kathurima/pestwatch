efb720d00f788815a8a84d64ab725dbc55e9a25b
Roy-Kathurima <roykimathi922@gmail.com>
2025-12-03 09:41:15 +0300

    Update config.py

--- pestwatch/config.py
+++ pestwatch/config.py
@@ -1,8 +1,9 @@
 import os
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "change-this"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(BASE_DIR, "database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", os.path.join(BASE_DIR, "uploads"))
+    MAX_CONTENT_LENGTH = 6 * 1024 * 1024  # 6 MB max upload
