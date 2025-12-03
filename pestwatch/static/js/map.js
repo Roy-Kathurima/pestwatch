0d69c1e5e6068790246fdf8ece1b063c193c886d
Roy-Kathurima <roykimathi922@gmail.com>
2025-12-03 03:29:34 +0300

    Update map.js

--- pestwatch/static/js/map.js
+++ pestwatch/static/js/map.js
@@ -1,10 +1,12 @@
 function loadMap(reports) {
   var map = L.map("map").setView([-1.28, 36.82], 6);
-
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
+  if (!Array.isArray(reports)) return;
   reports.forEach(r => {
-    L.marker([r.lat, r.lng]).addTo(map)
-      .bindPopup(r.title);
+    if (r.lat && r.lng) {
+      L.marker([r.lat, r.lng]).addTo(map)
+        .bindPopup((r.title || 'Report') + "<br>" + (r.created_at || ""));
+    }
   });
 }
